5c88081dff63b240c6c5e6f8bdcb17154f2e14d6
John Robling <roblingjohn@gmail.com>
2020-06-25 21:08:53 -0400

    wrote questions

--- index.js
+++ index.js
@@ -1,7 +1,56 @@
+const inquirer = require("inquirer");
+
 // array of questions for user
 const questions = [
+    {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please describe the project.",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Please explain any installation instructions.",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Please give info on the usage.",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "I need to figure out how to do the choosy thing.",
+        name: "license"
+    },
+    {
+        type: "input",
+        message: "Any contribution guidelines?",
+        name: "contribution"
+    },
+    {
+        type: "input",
+        mesaage: "Testing parameters?",
+        name: "testing"
+    },
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    }
 
 ];
+
+inquirer.prompt(questions);
 
 // function to write README file
 function writeToFile(fileName, data) {

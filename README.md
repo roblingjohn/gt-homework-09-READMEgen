a7fb8e61afc1bd84145ea8919d9a7ab6332d1a97
John Robling <roblingjohn@gmail.com>
2020-06-26 19:21:18 -0400

    added license badges and created a sample generated README

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# gt-homework-09-READMEgen
+
+# Good README Generator
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+## Description
+This project dynamically generates a README based on user input.
+
+## Table of Contents
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributiong](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+Use the command "npm i" to install dependencies.
+
+## Usage
+This is a standard repo.
+
+## License
+This project is licensed under the MIT license.
+
+## Contributing
+Please do not contribute.
+
+## Tests
+Use the command "npm test" to run tests.
+
+## Questions
+If you have any questions, you can email me directly at roblingjohn@gmail.com. 
+Please visit my GitHub page at [roblingjohn](http://github.com/roblingjohn).
+        
--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const questions = [
     },
     {
         type: "input",
-        message: "Please giver a short description of the project.",
+        message: "Please give a short description of the project.",
         name: "description"
     },
     {
@@ -60,29 +60,47 @@ const questions = [
 
 
 let READMEText;
+let licenseBadge;
 inquirer.prompt(questions)
 // put data from answers into variables
     .then(function(answer) {
         const {username, email, title, description, license, install, tests, usage, contribution} = answer
 // created html template with template literals for input answers
+        if (license === "MIT"){
+            licenseBadge = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        }
+        else if (license === "APACHE 2.0") {
+            licenseBadge = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        }
+        else if (license === "GPL 3.0") {
+            licenseBadge = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+        }
+        else if (license === "BSD 3") {
+            licenseBadge = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+        }
+        else if (license === "None") {
+            licenseBadge = ""
+        }
+
         READMEText = `
 # ${title}
 
+${licenseBadge}
 ## Description
 ${description}
 
 ## Table of Contents
 * [Installation](#installation)
 
-*[Usage](#usage)
+* [Usage](#usage)
 
-*[License](#license)
+* [License](#license)
 
-*[Contributiong](#contributing)
+* [Contributiong](#contributing)
 
-*[Tests](#tests)
+* [Tests](#tests)
 
-*[Questions](#questions)
+* [Questions](#questions)
 
 ## Installation
 Use the command "${install}" to install dependencies.
@@ -100,9 +118,10 @@ ${contribution}
 Use the command "${tests}" to run tests.
 
 ## Questions
-For any questions, please contact me at http://github.com/${username} or at ${email}.
+If you have any questions, you can email me directly at ${email}. 
+Please visit my GitHub page at [${username}](http://github.com/${username}).
         `
-}).then(function () {fs.writeFile("sampleREADME.md", READMEText, (err) => {
+}).then(function () {fs.writeFile("README.md", READMEText, (err) => {
     if (err) throw err;
 })
 }
